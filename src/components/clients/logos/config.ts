a46e38e1f98b4b2fbf357eecd773018af18a3827
deamsf <frankdamen@gmail.com>
2025-01-25 17:35:12 +0100

    small fix client logo visibility

--- src/components/clients/logos/config.ts
+++ src/components/clients/logos/config.ts
@@ -3,12 +3,12 @@ import type { ClientLogo } from './types';
 export const logos: ClientLogo[] = [
   {
     name: 'Swirlazzo',
-    svgPath: '/src/components/clients/logos/swirlazzo-logo-zwart.svg',
+    svgPath: '/img/clientlogos/swirlazzo-logo-zwart.svg',
     website: 'https://www.swirlazzo.com'
   },
   {
     name: 'Jeroen Jeuris',
-    svgPath: '/src/components/clients/logos/logo-jj.svg',
+    svgPath: '/img/clientlogos/logo-jj.svg',
     website: 'https://www.jeroenjeuris.be'
   }
 ];
