ad63cf9cdf09a318b5aeb11b4280082aff7d7993
deamsf <frankdamen@gmail.com>
2025-04-23 00:35:10 +0200

    Delete src directory

--- src/constants/navigation.ts
+++
@@ -1,8 +0,0 @@
-export const navigation = {
-  links: [
-    { label: 'Wat ik doe', href: '#services' },
-    { label: 'Over mij', href: '#about' },
-    { label: 'Klanten', href: '#clients' },
-    { label: 'Contact', href: '#contact' }
-  ]
-} as const;
